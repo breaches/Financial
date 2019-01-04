b66f1cfdeb843be1210db637cd4c1ffee428c877
shaokang <1206751678@qq.com>
2019-01-04 10:35:01 +0800

    shaokang test toHome

--- admin/src/main/java/com/breach/huajinbao/controller/base/MapController.java
+++ admin/src/main/java/com/breach/huajinbao/controller/base/MapController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MapController {
-	
+
+	// shaokang
 	@RequestMapping("/toHome")
 	public String toHome(){
 		return "base/home";
