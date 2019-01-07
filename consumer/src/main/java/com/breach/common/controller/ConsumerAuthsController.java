5ad061f85bd13beddd059286e75eb3b0750d1a55
shaokang <1206751678@qq.com>
2019-01-07 23:36:14 +0800

    清理垃圾 Mapper.xml 文件

---
+++ consumer/src/main/java/com/breach/common/controller/ConsumerAuthsController.java
@@ -0,0 +1,24 @@
+package com.breach.common.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户登录授权表
+目前两种方式直接进行登录：
+用户名
+手机号码 前端控制器
+ * </p>
+ *
+ * @author shaokang
+ * @since 2019-01-07
+ */
+@Controller
+@RequestMapping("/consumerAuths")
+public class ConsumerAuthsController {
+
+}
+
---
+++ consumer/src/main/java/com/breach/common/service/IConsumerAuthsService.java
@@ -0,0 +1,19 @@
+package com.breach.common.service;
+
+import com.breach.common.entity.ConsumerAuths;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户登录授权表
+目前两种方式直接进行登录：
+用户名
+手机号码 服务类
+ * </p>
+ *
+ * @author shaokang
+ * @since 2019-01-07
+ */
+public interface IConsumerAuthsService extends IService<ConsumerAuths> {
+
+}
